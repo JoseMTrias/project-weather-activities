788fa0b62f1c5f1ad62b08fd852e6b2d9224e42e
JoseMTrias <jmgarciatrias@gmail.com>
2023-02-14 10:46:09 +0100

    handle the submit event

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
 import Form from './components/Form';
 import './App.css';
+import useLocalStorageState from 'use-local-storage-state';
+import { uid } from 'uid';
+
+const initialActivity = [
+  {
+    id: 1,
+    dateAdded: 'June 15, 2046',
+    activity: 'surfing',
+    weather: 'bad',
+    notes: "don't forget the neopren",
+  },
+];
 
 function App() {
+  const [activiy, setActivity] = useLocalStorageState('activityStorage', {
+    defaultValue: initialActivity,
+  });
+
+  const id = uid(3);
+  const options = { month: 'short', day: 'numeric', year: 'numeric' };
+  const dateAdded = new Date().toLocaleDateString('en-us', options);
+
+  function onAddActivity({ activity, notes, weather }) {
+    setActivity([{ id, dateAdded, activity, notes, weather }, ...activity]);
+  }
+
   return (
     <>
       <header></header>
       <main>
-        <Form />
+        <Form onAddActivity={onAddActivity} />
       </main>
       <footer></footer>
     </>
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,9 +1,22 @@
-export default function Form() {
+export default function Form({ onAddActivity }) {
+  function handleAddActivity(event) {
+    event.preventDefault();
+    const newActivityElement = new FormData(event.target);
+    const newActivity = Object.fromEntries(newActivityElement);
+
+    onAddActivity(newActivity);
+
+    event.target.reset();
+    document.getElementById('activity').focus();
+  }
+
   return (
-    <form>
+    <form onSubmit={handleAddActivity}>
       <h3 aria-label="form title element">Form element</h3>
       <label htmlFor="activity">Name of Activity</label>
       <input type="text" name="activity" id="activity"></input>
+      <label htmlFor="notes">Notes of Activity</label>
+      <input type="text" name="notes" id="notes"></input>
       <label htmlFor="weather">Good-bad weather checkbox</label>
       <input type="checkbox" name="weather" id="weather"></input>
       <button type="submit" onAddActivity={onAddActivity}>
